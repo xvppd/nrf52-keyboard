efb7c8468069d1a5b5c066128336909051902edb
xvppd <45332889+xvppd@users.noreply.github.com>
2024-12-13 22:07:18 +0800

    Update keyboard_evt.c

--- application/main/src/keyboard/keyboard_evt.c
+++ application/main/src/keyboard/keyboard_evt.c
@@ -108,9 +108,6 @@ static void internal_event_handler(enum user_event event, void* arg)
         if (subEvent == SLEEP_EVT_MANUAL) {
             // 手动休眠时，设置标志位0，下次按键启动
             sleep_reason_set(false);
-        } else {
-            // 手动关机或自动休眠时，设置标志位1，下次免按键启动
-            sleep_reason_set(true);
         }
         break;
 #ifdef PIN_CHARGING
